89b4139cab0cb9098756589760c5035b6719652f
nkhatri7 <79176897+nkhatri7@users.noreply.github.com>
2022-08-09 18:02:36 +1000

    initial commit

---
+++ log_parser.py
@@ -0,0 +1,8 @@
+# Log Parser
+# log_parser.py
+#
+# Written by Neil Khatri
+# on 09/08/2022
+#
+# This program parses a log file and prints out the number of unique
+# IP addresses, the top 3 most visited URLs and top 3 most active IP addresses
